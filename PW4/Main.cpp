b42d5f83eb77976614c7103e12c14ec04f896ec8
yaetoti <turkmencheburek0.o@gmail.com>
2023-11-13 22:05:33 +0200

    Configure ConsoleLib submodule.

--- PW4/Main.cpp
+++ PW4/Main.cpp
@@ -1,5 +1,8 @@
 #include <Windows.h>
+#include <Console.h>
 
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) {
-
+	Console::GetInstance()->WPrintF(L"Hello, PW4\n");
+	Console::GetInstance()->Pause();
+	return 0;
 }
