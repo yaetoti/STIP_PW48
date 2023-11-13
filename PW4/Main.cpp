5133415f79c36649212aab7fb800ef6e93717db7
yaetoti <turkmencheburek0.o@gmail.com>
2023-11-14 01:24:24 +0200

    Add LSZZ functions for PW4. Add file I/O functions.

--- PW4/Main.cpp
+++ PW4/Main.cpp
@@ -1,8 +1,135 @@
+#define NOMINMAX
 #include <Windows.h>
 #include <Console.h>
+#include <fstream>
+#include <string>
+#include <vector>
+
+constexpr const wchar_t* kInputFile = L"input.txt";
+constexpr const wchar_t* kEncodedFile = L"encoded.txt";
+constexpr const wchar_t* kDecodedFile = L"decoded.txt";
+
+typedef int lzss_size;
+
+const lzss_size WINDOW_SIZE = 4096;
+const lzss_size MIN_MATCH_SIZE = 1;
+const uint8_t LITERAL_MARKER = 0;
+const uint8_t MATCH_MARKER = 1;
+
+inline void VecEmplaceInt(std::vector<uint8_t>& vec, lzss_size value) {
+    for (size_t i = 0; i < sizeof(lzss_size); ++i) {
+        vec.emplace_back((uint8_t)((value >> (8 * i)) & 0xFF));
+    }
+}
+
+inline int VecGetInt(const std::vector<uint8_t>& vec, size_t index) {
+    lzss_size result = 0;
+    for (size_t i = 0; i < sizeof(lzss_size); ++i) {
+        result |= (lzss_size)(vec[index + i]) << (8 * i);
+    }
+
+    return result;
+}
+
+std::vector<uint8_t> LzssEncode(const std::vector<uint8_t>& input) {
+    std::vector<uint8_t> encoded;
+    lzss_size index = 0;
+
+    while (index < input.size()) {
+        lzss_size matchLength = 0;
+        lzss_size matchIndex = -1;
+
+        for (lzss_size i = std::max(0, index - WINDOW_SIZE); i < index; ++i) {
+            lzss_size j = 0;
+
+            while (index + j < input.size() && input[i + j] == input[index + j]) {
+                ++j;
+
+                if (j >= MIN_MATCH_SIZE && j > matchLength) {
+                    matchLength = j;
+                    matchIndex = i;
+                }
+            }
+        }
+
+        if (matchLength >= MIN_MATCH_SIZE) {
+            encoded.emplace_back(MATCH_MARKER);
+            VecEmplaceInt(encoded, matchIndex);
+            VecEmplaceInt(encoded, matchLength);
+            //encoded.emplace_back(matchIndex);
+            //encoded.emplace_back(matchLength);
+            index += matchLength;
+        }
+        else {
+            encoded.emplace_back(LITERAL_MARKER);
+            encoded.emplace_back(input[index++]);
+        }
+    }
+
+    return encoded;
+}
+
+std::vector<uint8_t> LzssDecode(const std::vector<uint8_t>& encoded) {
+    std::vector<uint8_t> decoded;
+    lzss_size index = 0;
+
+    while (index < encoded.size()) {
+        uint8_t marker = encoded[index++];
+
+        if (marker == LITERAL_MARKER) {
+            decoded.emplace_back(encoded[index++]);
+        }
+        else if (marker == MATCH_MARKER) {
+            //int matchIndex = encoded[index++];
+            //int matchLength = encoded[index++];
+            lzss_size matchIndex = VecGetInt(encoded, index);
+            index += sizeof(lzss_size);
+            lzss_size matchLength = VecGetInt(encoded, index);
+            index += sizeof(lzss_size);
+
+            for (lzss_size i = 0; i < matchLength; ++i) {
+                decoded.emplace_back(decoded[matchIndex + i]);
+            }
+        }
+    }
+
+    return decoded;
+}
+
+std::vector<uint8_t> ReadFile(const std::wstring& filename) {
+    std::ifstream input(filename, std::ios::binary);
+    if (!input.is_open()) {
+        throw std::runtime_error("Error opening file.");
+    }
+
+    input.seekg(0, std::ios::end);
+    std::streamsize fileSize = input.tellg();
+    input.seekg(0, std::ios::beg);
+
+    std::vector<uint8_t> buffer(static_cast<size_t>(fileSize));
+    if (!input.read((char*)buffer.data(), fileSize)) {
+        throw std::runtime_error("Error reading file.");
+    }
+
+    return buffer;
+}
+
+void WriteFile(const std::wstring& filename, const std::vector<uint8_t>& data) {
+    std::ofstream output(filename, std::ios::binary | std::ios::trunc);
+    if (!output.is_open()) {
+        throw std::runtime_error("Error opening file for writing.");
+    }
+
+    if (!output.write((const char*)data.data(), data.size())) {
+        throw std::runtime_error("Error writing to file.");
+    }
+}
 
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) {
-	Console::GetInstance()->WPrintF(L"Hello, PW4\n");
-	Console::GetInstance()->Pause();
+    std::vector<uint8_t> data = ReadFile(kInputFile);
+    std::vector<uint8_t> encoded = LzssEncode(data);
+    WriteFile(kEncodedFile, encoded);
+    std::vector<uint8_t> decoded = LzssDecode(encoded);
+    WriteFile(kDecodedFile, decoded);
 	return 0;
 }
