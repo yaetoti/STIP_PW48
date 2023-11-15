3f53002b40a053cd3e8a9e77cbeabe836a78c13b
yaetoti <turkmencheburek0.o@gmail.com>
2023-11-15 20:16:51 +0200

    Implement MD2 hashing.

--- PW7/Main.cpp
+++ PW7/Main.cpp
@@ -2,8 +2,17 @@
 #include <Console.h>
 #include <Utils.h>
 
+#include "HashMD2.h"
+
 int WINAPI wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) {
-	Console::GetInstance()->WPrintF(L"SUCK MY DICK!!\n");
+    std::string message = "";
+    std::vector<uint8_t> data(message.begin(), message.end());
+    std::vector<uint8_t> hash = HashMD2(data);
+
+    for (uint8_t byte : hash) {
+        Console::GetInstance()->WPrintF(L"%02x", byte);
+    }
+
 	Console::GetInstance()->Pause();
 	return 0;
 }
